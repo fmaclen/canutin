81426a6263bfa8b73dfe96bcd7d40cd85a65f715
Fernando Maclen <fmaclen@gmail.com>
2025-09-08 21:20:56 -0400

    feat: add basic table structure

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ import svelteConfig from './svelte.config.js';
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
+	{ ignores: ['pocketbase/pb_migrations/**'] },
 	includeIgnoreFile(gitignorePath),
 	js.configs.recommended,
 	...ts.configs.recommended,
