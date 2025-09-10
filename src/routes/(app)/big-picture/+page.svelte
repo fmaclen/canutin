566762a6ef128616e27097f2d0400d9ed81424a8
Fernando Maclen <fmaclen@gmail.com>
2025-09-10 17:57:53 -0400

    feat: add trailing-cashflow component

--- src/routes/(app)/big-picture/+page.svelte
+++ src/routes/(app)/big-picture/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import KeyValue from '$lib/components/key-value.svelte';
 	import SectionTitle from '$lib/components/section-title.svelte';
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import * as Tabs from '$lib/components/ui/tabs/index.js';
 	import { m } from '$lib/paraglide/messages';
 
 	import Summary from './summary.svelte';
+	import TrailingCashflow from './trailing-cashflow.svelte';
 </script>
 
 <header class="flex h-16 shrink-0 items-center gap-2 border-b">
@@ -31,32 +30,6 @@
 	</section>
 
 	<section class="mx-auto flex w-full flex-col space-y-2">
-		<Tabs.Root value="six-months">
-			<nav class="flex items-center justify-between space-x-2">
-				<SectionTitle title="Trailing cashflow" />
-
-				<Tabs.List>
-					<Tabs.Trigger value="three-months">3M</Tabs.Trigger>
-					<Tabs.Trigger value="six-months">6M</Tabs.Trigger>
-					<Tabs.Trigger value="nine-months">YTD</Tabs.Trigger>
-					<Tabs.Trigger value="one-year">1Y</Tabs.Trigger>
-					<Tabs.Trigger value="two-years">2Y</Tabs.Trigger>
-				</Tabs.List>
-			</nav>
-			<Tabs.Content value="three-months">
-				<div class="grid gap-2 lg:grid-cols-[1.3fr_1fr_1fr]">
-					<KeyValue title="Income per month" value={15000} />
-					<KeyValue title="Expenses per month" value={10000} />
-					<KeyValue title="Surplus per month" value={5000} />
-				</div>
-			</Tabs.Content>
-			<Tabs.Content value="six-months">
-				<div class="grid gap-2 lg:grid-cols-[1.3fr_1fr_1fr]">
-					<KeyValue title="Income per month" value={15000} />
-					<KeyValue title="Expenses per month" value={10000} />
-					<KeyValue title="Surplus per month" value={5000} />
-				</div>
-			</Tabs.Content>
-		</Tabs.Root>
+		<TrailingCashflow />
 	</section>
 </div>
---
+++ src/routes/(app)/big-picture/trailing-cashflow.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import * as Tabs from '$lib/components/ui/tabs/index.js';
+  import SectionTitle from '$lib/components/section-title.svelte';
+  import KeyValue from '$lib/components/key-value.svelte';
+</script>
+
+<Tabs.Root value="six-months">
+  <nav class="flex items-center justify-between space-x-2">
+    <SectionTitle title="Trailing cashflow" />
+
+    <Tabs.List>
+      <Tabs.Trigger value="three-months">3M</Tabs.Trigger>
+      <Tabs.Trigger value="six-months">6M</Tabs.Trigger>
+      <Tabs.Trigger value="nine-months">YTD</Tabs.Trigger>
+      <Tabs.Trigger value="one-year">1Y</Tabs.Trigger>
+      <Tabs.Trigger value="two-years">2Y</Tabs.Trigger>
+    </Tabs.List>
+  </nav>
+  <Tabs.Content value="three-months">
+    <div class="grid gap-2 lg:grid-cols-[1.3fr_1fr_1fr]">
+      <KeyValue title="Income per month" value={15000} />
+      <KeyValue title="Expenses per month" value={10000} />
+      <KeyValue title="Surplus per month" value={5000} />
+    </div>
+  </Tabs.Content>
+  <Tabs.Content value="six-months">
+    <div class="grid gap-2 lg:grid-cols-[1.3fr_1fr_1fr]">
+      <KeyValue title="Income per month" value={15000} />
+      <KeyValue title="Expenses per month" value={10000} />
+      <KeyValue title="Surplus per month" value={5000} />
+    </div>
+  </Tabs.Content>
+</Tabs.Root>
+
