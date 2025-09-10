397bcf32091a3b4c13a3fbb1f7c7b7ad28a446c5
Fernando Maclen <fmaclen@gmail.com>
2025-09-09 23:24:35 -0400

    fix: refine syntax for accounts context

--- src/lib/accounts.svelte.ts
+++ src/lib/accounts.svelte.ts
@@ -1,4 +1,4 @@
-import type PocketBase from 'pocketbase';
+import PocketBase, { type RecordSubscription } from 'pocketbase';
 import { getContext, setContext } from 'svelte';
 
 import type { AccountBalancesResponse, AccountsResponse } from './pocketbase.schema';
@@ -12,37 +12,47 @@ class AccountsContext {
 
 	constructor(pb: PocketBase) {
 		this._pb = pb;
-		this.getAll();
+		this.init();
+	}
+
+	private async init() {
+		await this.getAccounts();
+		await this.getAccountBalances();
 		this.realtimeSubscribe();
 	}
 
-	private async getAll() {
+	private async getAccounts() {
 		const list = await this._pb.collection('accounts').getFullList<AccountsResponse>();
+		const map = new Map<string, number>(this.accounts.map((a) => [a.id, a.balance]));
+		this.accounts = list.map((a) => ({ ...a, balance: map.get(a.id) ?? 0 }));
+	}
 
-		const withBalances: AccountWithBalance[] = [];
-		for (const a of list) {
-			const res = await this._pb
-				.collection('accountBalances')
-				.getList<AccountBalancesResponse>(1, 1, { filter: `account='${a.id}'`, sort: '-created' });
-			const value = res.items[0]?.value ?? 0;
-			withBalances.push({ ...a, balance: value } as AccountWithBalance);
+	private async getAccountBalance(accountId: string) {
+		const res = await this._pb
+			.collection('accountBalances')
+			.getList<AccountBalancesResponse>(1, 1, {
+				filter: `account='${accountId}'`,
+				sort: '-created'
+			});
+		const value = res.items[0]?.value ?? 0;
+		this.accounts = this.accounts.map((x) => (x.id === accountId ? { ...x, balance: value } : x));
+	}
+
+	private async getAccountBalances() {
+		for (const a of this.accounts) {
+			await this.getAccountBalance(a.id);
 		}
-		this.accounts = withBalances;
 	}
 
 	private realtimeSubscribe() {
-		this._pb.collection('accounts').subscribe('*', () => this.getAll());
-		this._pb.collection('accountBalances').subscribe('*', async (e) => {
-			const accountId = e.record.account as string;
-			const res = await this._pb
-				.collection('accountBalances')
-				.getList<AccountBalancesResponse>(1, 1, {
-					filter: `account='${accountId}'`,
-					sort: '-created'
-				});
-			const value = res.items[0]?.value ?? 0;
-			this.accounts = this.accounts.map((x) => (x.id === accountId ? { ...x, balance: value } : x));
-		});
+		this._pb.collection('accounts').subscribe('*', () => this.getAccounts());
+		this._pb
+			.collection('accountBalances')
+			.subscribe('*', (e: RecordSubscription<AccountBalancesResponse>) => {
+				const accountId = e.record.account;
+				if (!accountId) return;
+				this.getAccountBalance(accountId);
+			});
 	}
 
 	dispose() {
