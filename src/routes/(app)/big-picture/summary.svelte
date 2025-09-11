ffaf50e2eee9f49816e1d531219609c13e6df65e
Fernando Maclen <fmaclen@gmail.com>
2025-09-11 14:15:53 -0400

    chore: apply formatting

--- src/routes/(app)/big-picture/summary.svelte
+++ src/routes/(app)/big-picture/summary.svelte
@@ -20,7 +20,11 @@
 </script>
 
 <div class="text-background grid gap-2 lg:grid-cols-[1.3fr_1fr_1fr]">
-	<div class="flex flex-col justify-between rounded-sm bg-stone-700 p-5 shadow-md md:row-span-2" role="region" aria-label="Net worth">
+	<div
+		class="flex flex-col justify-between rounded-sm bg-stone-700 p-5 shadow-md md:row-span-2"
+		role="region"
+		aria-label="Net worth"
+	>
 		<div class="text-base font-semibold tracking-tight">Net worth</div>
 		<div class="font-mono text-xl">
 			<Currency value={totals.netWorth} />
