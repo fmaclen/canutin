9158a35804fbf1068659d58be21b660bfd692ec0
Fernando Maclen <fmaclen@gmail.com>
2025-09-13 10:27:33 -0400

    fix: add cascade-delte for asset/account balances

---
+++ pocketbase/pb_migrations/1757773158_updated_accountBalances.js
@@ -0,0 +1,40 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1811848958")
+
+  // update field
+  collection.fields.addAt(3, new Field({
+    "cascadeDelete": true,
+    "collectionId": "pbc_2324088501",
+    "hidden": false,
+    "id": "relation2100713124",
+    "maxSelect": 1,
+    "minSelect": 0,
+    "name": "account",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1811848958")
+
+  // update field
+  collection.fields.addAt(3, new Field({
+    "cascadeDelete": false,
+    "collectionId": "pbc_2324088501",
+    "hidden": false,
+    "id": "relation2100713124",
+    "maxSelect": 1,
+    "minSelect": 0,
+    "name": "account",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "relation"
+  }))
+
+  return app.save(collection)
+})
