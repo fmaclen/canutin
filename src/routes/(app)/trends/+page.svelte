ad9f2100d06603f8a15a151f3b91eee23b2b30d3
Fernando Maclen <fmaclen@gmail.com>
2025-09-12 22:58:30 -0400

    feat: add placeholder sections

---
+++ src/routes/(app)/trends/+page.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { m } from '$lib/paraglide/messages';
+</script>
+
+<header class='bg-background flex h-16 shrink-0 items-center gap-2 border-b'>
+	<div class='flex items-center gap-2 px-4'>
+		<Sidebar.Trigger class='-ml-1' />
+		<Separator orientation='vertical' class='mr-2 data-[orientation=vertical]:h-4' />
+		<Breadcrumb.Root>
+			<Breadcrumb.List>
+				<Breadcrumb.Item>
+					<Breadcrumb.Page>{m.sidebar_trends()}</Breadcrumb.Page>
+				</Breadcrumb.Item>
+			</Breadcrumb.List>
+		</Breadcrumb.Root>
+	</div>
+</header>
+
