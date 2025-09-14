474262f590c33cc2e488dcb6da8b29026e6a3e3b
Fernando Maclen <fmaclen@gmail.com>
2025-09-14 15:04:32 -0400

    fix: update thead apdding

--- src/lib/components/ui/table/table-head.svelte
+++ src/lib/components/ui/table/table-head.svelte
@@ -15,7 +15,7 @@
 	bind:this={ref}
 	data-slot="table-head"
 	class={cn(
-		'text-muted-foreground h-10 bg-clip-padding px-2 text-left align-middle text-xs font-normal whitespace-nowrap [&:has([role=checkbox])]:pr-0',
+		'text-muted-foreground h-10 bg-clip-padding px-4 py-2 text-left align-middle text-xs font-normal whitespace-nowrap [&:has([role=checkbox])]:pr-0',
 		className
 	)}
 	{...restProps}
