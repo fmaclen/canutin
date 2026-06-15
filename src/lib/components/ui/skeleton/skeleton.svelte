ccc78b99b8c450a040661d79999a8c57023a5369
Fernando Maclen <fmaclen@gmail.com>
2026-06-15 10:29:27 -0400

    feat: add spinner to skeletons

--- src/lib/components/ui/skeleton/skeleton.svelte
+++ src/lib/components/ui/skeleton/skeleton.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import LoaderCircleIcon from '@lucide/svelte/icons/loader-circle';
 	import type { HTMLAttributes } from 'svelte/elements';
 
 	import { cn, type WithElementRef, type WithoutChildren } from '$lib/utils.js';
@@ -13,6 +14,11 @@
 <div
 	bind:this={ref}
 	data-slot="skeleton"
-	class={cn('bg-border animate-pulse rounded', className)}
+	class={cn('bg-border relative overflow-hidden rounded', className)}
 	{...restProps}
-></div>
+>
+	<LoaderCircleIcon
+		aria-hidden="true"
+		class="text-muted-foreground/70 absolute top-1/2 left-1/2 size-4 -translate-x-1/2 -translate-y-1/2 animate-spin"
+	/>
+</div>
